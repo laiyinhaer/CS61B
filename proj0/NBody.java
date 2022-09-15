fd79b9c085a4fefa48f229046888dbfd3361e54c
yinhamu <yinhanu@gmail.com>
2022-09-15 18:50:43 +0800

    以后不要å再修改现成的skeleton代码了 proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -3,7 +3,7 @@ public class NBody {
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
-        double radius = ReadRadius(filename);
+        double radius = readRadius(filename);
         Planet[] planets = readPlanets(filename);
 
         String image = "images/starfield.jpg";
